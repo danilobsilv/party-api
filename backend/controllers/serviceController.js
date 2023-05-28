54d516cef3057bce0be97456fcc32e3f7d65074f
Danilo Bruno <danilobsilv@gmail.com>
2023-05-27 21:05:27 -0400

    service model

---
+++ backend/controllers/serviceController.js
@@ -0,0 +1,29 @@
+const {Service: ServiceModel} = require("../models/Service");
+
+const serviceController = {
+
+      create: async(req, res) => {
+            try {
+                  const service = {
+                        name: req.body.name,
+                        description: req.body.description,
+                        price: req.body.price,
+                        image: req.body.image
+                  };
+
+                  const response = await ServiceModel.create(service);
+                  
+                  res.status(201).json({response, msg:"Service created successfully!"})
+
+            } catch (error) {
+                  console.log(`ERROR: ${error}`);
+            }
+      }
+}
+
+
+module.exports = serviceController;
+
+
+// criar algo no banco --> 201
+// td certo --> 200     
