f38044bda33a231406e453177be4f74c47c64736
Danilo Bruno <danilobsilv@gmail.com>
2023-05-28 20:41:57 -0400

    add: parties router

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
-const servicesRouter = require("./services")
 
+// Services router
+const servicesRouter = require("./services");
 
 router.use("/", servicesRouter);
+
+
+// Parties router
+const partyRouter = require("./parties");
+
+router.use("/", partyRouter);
 
 
 module.exports = router;
