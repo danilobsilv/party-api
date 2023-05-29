957185572eb5ecb272ff416bfb485c8d1dba6c7f
Danilo Bruno <danilobsilv@gmail.com>
2023-05-28 20:43:04 -0400

    add: crud routes

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -18,10 +18,86 @@ const serviceController = {
             } catch (error) {
                   console.log(`ERROR: ${error}`);
             }
+      },
+      
+      getAll: async(req, res) => {
+            try {
+                  
+                  const services = await ServiceModel.find()
+
+                  res.json(services);
+
+            } catch (error) {
+                  console.log(`ERROR: ${error}`);
+            }
+      },
+
+      get: async(req, res) => {
+            try {
+                  
+                  const id = req.params.id;
+                  const services = await ServiceModel.findById(id);
+
+                  if (!services){
+                        res.status(404).send({msg: "Service not found."});
+                        return;
+                  }
+
+                  res.json(services);
+
+            } catch (error) {
+                  console.log(`Error: ${error}`);
+            }
+      },
+
+      delete: async(req, res) => {
+            try {
+                  
+                  const id = req.params.id;
+                  const services = await ServiceModel.findById(id);
+                  
+                  if (!services){
+                        res.status(404).send({msg: "Service not found."});
+                        return;
+                  }
+
+                  const deleteService = await ServiceModel.findByIdAndDelete(id);
+
+                  res.status(200).send({msg: "Service successfully deleted."});
+
+            } catch (error) {
+                  console.log(`Error: ${error}`);
+            }
+      },
+      
+      update: async(req, res) => {
+            try {
+                  
+                  const id = req.params.id;
+                  const service = {
+                        name: req.body.name,
+                        description: req.body.description,
+                        price: req.body.price,
+                        image: req.body.image
+                  };
+
+                  const updatedService = await ServiceModel.findByIdAndUpdate(id, service);
+                  
+                  if (!updatedService){
+                        res.status(404).send({msg: "Service not found."});
+                        return;
+                  }
+
+                  res.status(200).json({service, msg: "Service sucessfully updated."});
+
+            } catch (error) {
+                  console.log(`Error: ${error}`);
+            }
       }
-}
 
+};
 
-module.exports = serviceController;
+ 
+module.exports = serviceController
 // criar algo no banco --> 201
 // td certo --> 200     
